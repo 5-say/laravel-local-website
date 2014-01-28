9ec07e8b6b632b885ad0f57f045c8f458eaaf632
成武 <bcw.5@foxmail.com>
2014-01-28 09:39:11 +0800

    优化 welcome 页面载入速度

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -13,43 +13,42 @@
     <meta name="keywords" content="laravel, php, framework, web, artisans, taylor otwell">
 
     <!-- favicon -->
-    <link rel="shortcut icon" href="/favicon.png?v=2">
+    <link rel="shortcut icon" href="{{ asset('favicon.png?v=2') }}">
 
     <!-- we're minifying and combining all our css -->
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="{{ asset('/') }}assets/css/style.css" rel="stylesheet">
 
     <!-- grab jquery from google cdn. fall back to local if offline -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/jquery.js"><\/script>')</script>
+    <script src="{{ asset('/') }}assets/js/jquery.js"></script>
 
     <!-- prettyprint -->
-    <script src="http://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+    <script src="{{ route('run_prettify.js') }}"></script>
 
     <!-- load up our js -->
-    <script src="assets/js/plugins.js"></script>
-    <script src="assets/js/application.js"></script>
+    <script src="{{ asset('/') }}assets/js/plugins.js"></script>
+    <script src="{{ asset('/') }}assets/js/application.js"></script>
 
     <!-- fonts -->
-    <script src="http://use.edgefonts.net/source-sans-pro:n3,i3,n4,i4,n6,i6,n7,i7.js"></script>
-    <script src="http://use.edgefonts.net/source-code-pro.js"></script>
+    <link href="{{ asset('/') }}assets/css/source-sans-pro-n3-i3-n4-i4-n6-i6-n7-i7.js.css" rel="stylesheet">
+    <link href="{{ asset('/') }}assets/css/source-code-pro.js.css" rel="stylesheet">
 
     <!-- some conditionals for ie -->
-    <!--[if IE]><link href="assets/css/ie.css" rel="stylesheet" type="text/css" /><![endif]-->
+    <!--[if IE]><link href="{{ asset('/') }}assets/css/ie.css" rel="stylesheet" type="text/css" /><![endif]-->
 
     <!-- HTML5 elements in less than IE9, yes please! -->
-    <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="{{ asset('/') }}assets/js/html5.js"></script><![endif]-->
 
     <!-- If less than IE8 add some JS for the webfont icons -->
-    <!--[if lt IE 8]><script src="assets/js/ie_font.js"></script><![endif]-->
+    <!--[if lt IE 8]><script src="{{ asset('/') }}assets/js/ie_font.js"></script><![endif]-->
 
     <!-- asynchronous google analytics. change UA-XXXXX-X to your site's ID -->
     <script>
-        var _gaq=[['_setAccount','UA-23865777-1'],['_trackPageview']];
-        (function(d,t){
-    		var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-        	s.parentNode.insertBefore(g,s)
-    	}(document,'script'));
+        // var _gaq=[['_setAccount','UA-23865777-1'],['_trackPageview']];
+        // (function(d,t){
+        //     var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        //     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        //     s.parentNode.insertBefore(g,s)
+        // }(document,'script'));
     </script>
 </head>
 
